205d99b61c8f24864e36b6591b0174688fdd1cbe
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-12-14 16:40:51 +0100

    Actialización contenido de la card de evento en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,14 +14,19 @@
                     <!-- <img src="{{ asset('storage/' . $event->main_image) }}" alt="{{ $event->name }}"> -->
                     <img src="https://picsum.photos/2000" alt="{{ $event->name }}">
                 @endif
-                <p>{{ $event->description }}</p>
-                <p>Category: {{ $event->category->name }}</p>
-                <p>Venue: {{ $event->venue->name }}</p>
-                <p>Date: {{ \Carbon\Carbon::parse($event->event_date)->format('Y-m-d') }}</p>
-                <p>Lowest Ticket Price: {{ $event->lowestTicketPrice() }}</p>
+                <div class="card-content">
+                    <h3>{{ $event->name }}</h3>
+                    <p class="description">{{ $event->description }}</p>
+                    <p>Date: {{ \Carbon\Carbon::parse($event->event_date)->format('d-m-Y') }}</p> 
+                    <p>Venue: {{ $event->venue->name }}</p>
+                    <p>Lowest Ticket Price: {{ $event->lowestTicketPrice() }}</p>
+                    
+                </div>
+                
             </div>
         </a>
         @endforeach
     </div>
     {{ $events->links('vendor.pagination.bootstrap-4') }}
 @endsection
+
