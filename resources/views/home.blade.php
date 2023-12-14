b428dfa2de7975feffce40aea2112c14b874f4a7
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2023-12-14 18:31:30 +0100

    arreglar homePage con pagiacion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,26 +7,25 @@
 
     <div class="grid-container">
         @foreach ($events as $event)
-        <a class="card-link" href="{{ route('tickets.showevent') }}">
-            <div class="card">
-                
-                @if ($event->main_image)
-                    <!-- <img src="{{ asset('storage/' . $event->main_image) }}" alt="{{ $event->name }}"> -->
-                    <img src="https://picsum.photos/2000" alt="{{ $event->name }}">
-                @endif
-                <div class="card-content">
-                    <h3>{{ $event->name }}</h3>
-                    <p class="description">{{ $event->description }}</p>
-                    <p>Date: {{ \Carbon\Carbon::parse($event->event_date)->format('d-m-Y') }}</p> 
-                    <p>Venue: {{ $event->venue->name }}</p>
-                    <p>Lowest Ticket Price: {{ $event->lowestTicketPrice() }}</p>
-                    
+            <a class="card-link" href="{{ route('tickets.showevent') }}">
+                <div class="card">
+
+                    @if ($event->main_image)
+                        <!-- <img src="{{ asset('storage/' . $event->main_image) }}" alt="{{ $event->name }}"> -->
+                        <img src="https://picsum.photos/2000" alt="{{ $event->name }}">
+                    @endif
+                    <div class="card-content">
+                        <h3>{{ $event->name }}</h3>
+                        <p class="description">{{ $event->description }}</p>
+                        <p>Date: {{ \Carbon\Carbon::parse($event->event_date)->format('Y-m-d') }}</p>
+                        <p>Venue: {{ $event->venue->name }}</p>
+                        <p>Lowest Ticket Price: {{ $event->lowestTicketPrice() }}</p>
+
+                    </div>
+
                 </div>
-                
-            </div>
-        </a>
+            </a>
         @endforeach
     </div>
     {{ $events->links('vendor.pagination.bootstrap-4') }}
 @endsection
-
