afeaccfdaa0bb58931a166889636ea74bd517bb5
Guiu Puigantell Jordan <gpuigantell@alumnat.copernic.cat>
2023-12-15 14:59:04 +0000

    tests filtros

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+
+<form action="{{ route('home') }}" method="GET">
+    <div class="contenedorFiltro">
+        <label for="filtro">Buscar por:</label>
+        <select id="filtro" name="filtro" class="filtro">
+            <option value="ciudad" {{ $selectedFiltro == 'ciudad' ? 'selected' : '' }}>Ciudad</option>
+            <option value="recinto" {{ $selectedFiltro == 'recinto' ? 'selected' : '' }}>Recinto</option>
+            <option value="evento" {{ $selectedFiltro == 'evento' ? 'selected' : '' }}>Evento</option>
+        </select>
+        <input type="text" name="search" value="{{ $searchTerm }}">
+        <button type="submit" class="fas fa-search iconoLupa"></button>
+    </div>
+    <div class="contenedorFiltro">
+        <label for="categoria">Categoría:</label>
+        <select id="categoria" name="categoria" class="filtro">
+            <option value="todas">todas</option>
+            <option value="musica">musica</option>
+            <option value="magia">magia</option>
+            <option value="baile">baile</option>
+        </select>
+    </div>
+</form>
     <div class="pagination-info">
         <p>Mostrant {{ $events->firstItem() }} - {{ $events->lastItem() }} de {{ $events->total() }} events</p>
     </div>
