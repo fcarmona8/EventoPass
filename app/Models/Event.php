a6081c15ca286b1c72b06513dfa96bc19b4f0731
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2023-12-19 18:27:33 +0100

    Logs actualizados con informacion importante en HomeController y EventModel

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -15,21 +15,26 @@ class Event extends Model
 
     protected $dates = ['event_date'];
 
-    public function category() { 
+    public function category() {
+        Log::info('Llamada al método category', ['event_id' => $this->id]);
         return $this->belongsTo(Category::class); 
     }
 
-    public function venue() { 
+    public function venue() {
+        Log::info('Llamada al método venue', ['event_id' => $this->id]);
         return $this->belongsTo(Venue::class); 
     }
 
     public function sessions()
     {
+        Log::info('Llamada al método sessions', ['event_id' => $this->id]);
         return $this->hasMany(Session::class);
     }
 
     public function lowestTicketPrice()
     {
+        $startTime = microtime(true);
+
         try {
             $lowestPrice = $this->sessions()
                                 ->join('purchases', 'sessions.id', '=', 'purchases.session_id')
@@ -37,13 +42,15 @@ class Event extends Model
                                 ->join('ticket_types', 'tickets.type_id', '=', 'ticket_types.id')
                                 ->min('ticket_types.price');
 
+            $endTime = microtime(true);
+            Log::info('Tiempo de ejecución de lowestTicketPrice', ['duration' => $endTime - $startTime]);
+
             return $lowestPrice;
         } catch (\Exception $e) {
             Log::error('Error en la consulta de precio más bajo en el modelo Event', [
                 'event_id' => $this->id,
                 'error_message' => $e->getMessage()
             ]);
-
         }
     }
 
