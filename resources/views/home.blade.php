857c8b4b6482e65685085dffaefe2cff0f9a42e1
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-12-19 16:57:37 +0100

    Cambios estilos filtros, menu lateral desplegable en mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,41 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 
-<form action="{{ route('home') }}" method="GET">
-    <div class="contenedorFiltro">
-        <label for="filtro">Buscar per:</label>
-        <select id="filtro" name="filtro" class="filtro">
-            <option value="ciudad" {{ $selectedFiltro == 'ciudad' ? 'selected' : '' }}>Ciutat</option>
-            <option value="recinto" {{ $selectedFiltro == 'recinto' ? 'selected' : '' }}>Recinte</option>
-            <option value="evento" {{ $selectedFiltro == 'evento' ? 'selected' : '' }}>Nom</option>
-        </select>
-        <input type="text" name="search" value="{{ $searchTerm }}">
-        <button type="submit" class="fas fa-search iconoLupa"></button>
-    </div>
-    <div class="contenedorFiltro">
-        <label for="categoria">Categoria:</label>
-        <select id="categoria" name="categoria" class="filtro">
-            @foreach ($categories as $id => $name)
-                <option value="{{ $id }}" {{ $selectedCategoria == $id ? 'selected' : '' }}>{{ $name }}</option>
-            @endforeach
-        </select>
-    </div>
-</form>
+<button id="mostrarFiltros"> <i class="fas fa-filter iconoFiltrar"></i>Filtrar</button>
+
+<div class="barraLateral" id="barraLateral">
+    <button id="cerrarFiltros" ><i class="fas fa-times iconoCerrar"></i></button>
+    <form action="{{ route('home') }}" method="GET" class="filterForm">
+        <div class="contenedorFiltro">
+            <label for="filtro">Buscar per:</label>
+            <div class="contenedor-buscador">
+                <select id="filtro" name="filtro" class="filtro">
+                <option value="ciudad" {{ $selectedFiltro == 'ciudad' ? 'selected' : '' }}>Ciutat</option>
+                <option value="recinto" {{ $selectedFiltro == 'recinto' ? 'selected' : '' }}>Recinte</option>
+                <option value="evento" {{ $selectedFiltro == 'evento' ? 'selected' : '' }}>Nom</option>
+            </select>
+            <input type="text" name="search" value="{{ $searchTerm }}" placeholder="Escriu per cercar...">
+            </div>
+            
+            
+        </div>
+        <div id="contenedor-filtro-categoria" class="contenedorFiltro">
+            <label for="categoria">Categoria:</label>
+            <select id="categoria" name="categoria" class="filtro">
+                @foreach ($categories as $id => $name)
+                    <option value="{{ $id }}" {{ $selectedCategoria == $id ? 'selected' : '' }}>{{ $name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="contenedor-boton-buscar">
+            <button type="submit" class="fas fa-search iconoLupa"> <span>Buscar</span> </button>
+        </div>
+        
+    </form>
+</div>
+
+
+
     <div class="pagination-info">
         <p>Mostrant {{ $events->firstItem() }} - {{ $events->lastItem() }} de {{ $events->total() }} events</p>
     </div>
@@ -49,4 +64,22 @@
         @endforeach
     </div>
     {{ $events->links('vendor.pagination.bootstrap-4') }}
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+    const mostrarFiltrosBtn = document.getElementById('mostrarFiltros');
+    const cerrarFiltrosBtn = document.getElementById('cerrarFiltros');
+    const barraLateral = document.getElementById('barraLateral');
+
+    mostrarFiltrosBtn.addEventListener('click', function() {
+        // Mostrar la barra lateral
+        barraLateral.style.right = '0';
+    });
+
+    cerrarFiltrosBtn.addEventListener('click', function() {
+        // Ocultar la barra lateral
+        barraLateral.style.right = '-350px';
+    });
+});
+    </script>
 @endsection
