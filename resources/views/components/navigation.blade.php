ca0cea2d3fd5e273417182a0730240fb95cbcf81
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2024-01-11 16:31:41 +0100

    Arreglado ruta al login o home promotor según autentificacion

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,16 +1,3 @@
-@php
-    $routes = [
-        'home' => 'Home',
-        'resultats' => 'Resultats',
-        'tickets.promoterhome' => 'Home Promotor',
-        'tickets.showevent' => 'Mostrar Esdeveniment',
-        'tickets.buytickets' => 'Comprar Entrades',
-        'tickets.aboutus' => 'Sobre Nosaltres',
-        'tickets.legalnotice' => 'Avisos Legals',
-    ];
-    $activeRoute = Route::currentRouteName();
-@endphp
-
 <nav>
     <ul>
         <li><a href="{{ route('home') }}">Home</a></li>
@@ -23,11 +10,13 @@
             @if (Auth::user()->role->name == 'promotor')
                 <li><a href="{{ route('promotor.promoterhome') }}">Home Promotor</a></li>
                 <li><a href="{{ route('promotor.createEvent') }}">Crear Esdeveniment</a></li>
+            @else
+                <li><a href="{{ route('login') }}">Accés Promotors</a></li>
             @endif
 
             <li><a href="{{ route('user.profile') }}">Perfil d'Usuari</a></li>
         @else
-            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('login') }}">Accés Promotors</a></li>
         @endauth
 
         <li><a href="{{ route('tickets.aboutus') }}">Sobre Nosaltres</a></li>
