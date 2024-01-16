94588d0288eb50b4ad4be6481774f2a0996e3a01
Alfred Pérez Herranz <aperez@alumnat.copernic.cat>
2024-01-16 21:25:26 +0100

    API Maps recibe solicitudes pero hay que pagar

--- app/Http/Controllers/ShowEventController.php
+++ app/Http/Controllers/ShowEventController.php
@@ -15,21 +15,32 @@ class ShowEventController extends Controller
 {
     function getCoordinates($venue)
     {
+        // Inicialización del registro
+        Log::info("Iniciando la obtención de coordenadas para: Provincia - {$venue->province}, Ciudad - {$venue->city}, Código Postal - {$venue->postal_code}");
+    
         $apiKey = 'AIzaSyCbSv4bCYfNwXa_MXnzon8gG2kK_1MpoZw';
-        $address = urlencode("{$venue->city}, {$venue->province}");
+        $address = urlencode("{$venue->province}, {$venue->city}, {$venue->postal_code}");
         $url = "https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=$apiKey";
-
+    
+        // Registro de la URL solicitada
+        Log::debug("URL de solicitud de la API de Google Maps: $url");
+    
         $response = file_get_contents($url);
         $data = json_decode($response);
-
-        if(isset($data->results[0])){
+    
+        // Registro de la respuesta cruda de la API
+        Log::debug("Respuesta cruda de la API de Google Maps: " . print_r($data, true));
+    
+        if (isset($data->results[0])) {
+            // Registro de éxito
+            Log::info("Coordenadas encontradas: " . print_r($data->results[0]->geometry->location, true));
             return $data->results[0]->geometry->location;
         } else {
-            // Manejo de errores
-            Log::error("No se encontraron coordenadas para la dirección dada.");
+            // Registro de error
+            Log::error("No se encontraron coordenadas para la dirección dada. Respuesta de la API: " . print_r($data, true));
             return null;
         }
-    }
+    }    
 
     public function show($id)
     {
