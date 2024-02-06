5d6751bac20b27ddfb26a32a082096be2968df6d
= <=>
2024-02-06 21:48:28 +0100

    correu amb entrades tambe te enllac al event

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Mail;
 class MailController extends Controller{
 
 
-    public static function enviarEntrades($correu, $namePdf, $nameEvent){
+    public static function enviarEntrades($correu, $namePdf, $nameEvent, $eventId){
 
         $name = "http://127.0.0.1:8000/entrades/".$namePdf.".pdf";
 
-        Mail::to($correu)->send(new mailEntradesCorreu($name, $nameEvent));
+        $event = "http://127.0.0.1:8000/tickets/showevent/".$eventId;
+
+        Mail::to($correu)->send(new mailEntradesCorreu($name, $nameEvent, $event));
     }
 
 
