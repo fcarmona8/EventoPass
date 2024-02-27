6322210ac69999746f0dfae10d197b34a79d8647
= <=>
2024-02-27 16:36:43 +0100

    creada funcionalidad caducidad correos reastablecer contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     @include('partials.meta', $metaData ?? [])
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
